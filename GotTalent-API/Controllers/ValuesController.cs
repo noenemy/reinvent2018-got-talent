7ebfb1be205d060cdb05cdb82368a40ab1d3ffe0
noenemy <noenemy@gmail.com>
2018-09-21 00:32:16 +0900

    Allow CORS for calling APIs

--- GotTalent-API/Controllers/ValuesController.cs
+++ GotTalent-API/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using GotTalent_API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GotTalent_API.Controllers
 {
--- GotTalent-API/Startup.cs
+++ GotTalent-API/Startup.cs
@@ -31,6 +31,7 @@ namespace GotTalent_API
             string connectionString = Helpers.GetRDSConnectionString(this.Configuration);
             services.AddDbContext<DataContext>(x => x.UseMySql(connectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,7 @@ namespace GotTalent_API
             }
 
            //app.UseHttpsRedirection();
+           app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
