c4f405a692310785e454e65d8ffceb912646f125
noenemy <noenemy@gmail.com>
2018-09-21 00:03:33 +0900

    Add GotTalent-Web project created with Angular

--- GotTalent-API/Controllers/ValuesController.cs
+++ GotTalent-API/Controllers/ValuesController.cs
@@ -21,17 +21,17 @@ namespace GotTalent_API.Controllers
 
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.MyValues.ToList();
+            var values = await _context.MyValues.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.MyValues.FirstOrDefault(x => x.Id == id);
+            var value = await _context.MyValues.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
