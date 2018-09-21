5e7d2ee59b70581bd0d97279bdd6560f5cfe1269
noenemy <noenemy@gmail.com>
2018-09-21 23:25:39 +0900

    Remove unneccesary value component

--- GotTalent-Web/src/app/app.module.ts
+++ GotTalent-Web/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       RankingComponent,
       NavComponent
    ],
