02fd719a5c8a81b5f3ada9a149607d2de993e3eb
noenemy <noenemy@gmail.com>
2018-09-21 23:02:43 +0900

    Add nav component to web project

--- GotTalent-Web/src/app/app.module.ts
+++ GotTalent-Web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { RankingComponent } from './ranking/ranking.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      RankingComponent
+      RankingComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
