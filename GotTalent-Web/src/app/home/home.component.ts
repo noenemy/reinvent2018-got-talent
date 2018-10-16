9454e03959c8dc6627ea1ac9f49bdc1e1764216c
noenemy <noenemy@gmail.com>
2018-10-16 22:32:58 +0900

    Add GSAP library to animate UI elements

--- GotTalent-Web/src/app/home/home.component.ts
+++ GotTalent-Web/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Power1, Bounce } from 'gsap/all';
+declare var TweenMax: any;
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('gamelogo') box: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.animate();
   }
 
+  animate(): void {
+    TweenMax.fromTo('h1', 3, {x:300}, {x:10});
+    TweenMax.fromTo('#p1', 2.4, {x:300}, {x:10});
+    TweenMax.fromTo('#p2', 3.3, {x:300}, {x:10});
+  }
 }
