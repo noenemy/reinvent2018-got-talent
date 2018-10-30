1d0ba5fe85f4b8227c727633daa7b71304b0ae46
noenemy <noenemy@gmail.com>
2018-10-30 11:45:32 +0900

    Update routing info for debug component

--- GotTalent-Web/src/app/routes.ts
+++ GotTalent-Web/src/app/routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { GameComponent } from './game/game.component';
+import { DebugComponent } from './debug/debug.component';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'leaderboard', component: RankingComponent },
     { path: 'game', component: GameComponent },
+    { path: 'debug', component: DebugComponent },
     { path: 'aboutus', component: AboutusComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
